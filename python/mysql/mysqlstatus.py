b0428e371830d84d0cc42aea584c49879467f8ac
Serhii Kolomiiets <skolomiiets@mindk.com>
2015-08-27 18:56:14 +0300

    adding ConfigParser module

--- python/mysql/mysqlstatus.py
+++ python/mysql/mysqlstatus.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python
 #importing modules
-import MySQLdb
+import MySQLdb, ConfigParser, os, time
 
 #parsing configfile for credentials
-storage = open('myconfig')
-for lines in storage:
-    if 'host' in lines:	
-	my_host = lines.split(' ')[-1]
-    elif 'port' in lines:
-	my_port = lines.split(' ')[-1]
-    elif 'user' in lines:
-	my_user = lines.split(' ')[-1]
-    elif 'password' in lines:
-	my_password = lines.split(' ')[-1]
-#print host, port, user, password
+config = ConfigParser.ConfigParser()
+config.read("myconfig")
+my_username = config.get('mysql', 'user')
+my_password = config.get('mysql', 'password')
+my_hostname = config.get('mysql', 'host')
+my_port = config.get('mysql', 'port')
+#print hostname, port, username, password
 
 #connecting to mysql
-db = MySQLdb.connect(host=my_host,port=my_port,user=my_user,passwd=my_password)
-c=db.cursor()
-c.execute('SHOW FULL PROCESSLIST')
-c.fetchchone()
+if my_port > 0:
+    db = MySQLdb.connect(host = my_hostname, user = my_username, passwd = my_password, port = int(my_port))
+else:
+    db = MySQLdb.connect(host = my_hostname, user = my_username, passwd = my_password)
+while 1:
+    os.system('clear')
+    c=db.cursor()
+    c.execute('SHOW FULL PROCESSLIST')
+#    print c.fetchone()
+#    for row in c.fetchall():
+#	print row[0], " ", row[1]
